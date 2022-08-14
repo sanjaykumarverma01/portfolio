b9e424f63f7ea99a0f6c52ff1d74bb547efc607d
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2022-08-14 19:46:37 +0530

    Added Profile pic

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,22 +1,44 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import React from "react";
 
 const About = () => {
   return (
     <Box>
-      <Text fontSize="4xl" mt="2rem" fontWeight="600">
+      <Text fontSize="4xl" mt="5rem" fontWeight="600">
         About Me
       </Text>
-      <Flex>
-        <Box>
-          <Text>
-            Hi Everyone, I am <span>Sanjaykumar Verma</span> from{" "}
-            <span>Sultanpur, U.P.</span>
+      <Flex width="80%" margin="auto" textAlign="justify" mt="3rem">
+        <Box width="40%">
+          <Text fontSize="xl" >
+            Hi Everyone, I am{" "}
+            <span style={{ fontWeight: "bold" }}>Sanjaykumar Verma</span> from{" "}
+            <span style={{ fontWeight: "bold" }}>Sultanpur, U.P.</span>
           </Text>
 
-          <Text></Text>
+          <Text fontSize="xl">
+            Web Developer who loves to transform ideas into reality using my web
+            developement tools though coding
+          </Text>
+          <Text fontSize="xl">
+            Motivated developer with expertise in creating custom websites with
+            front-end and back-end languages, responsive frameworks and
+            database.
+          </Text>
+          <Text fontSize="xl">
+            Apart from coding, some other activities that I love to do!
+          </Text >
+          <Text ml="1rem" fontSize="xl">
+            <UnorderedList>
+              <ListItem>Playing Video Games</ListItem>
+              <ListItem>Making Vlogs</ListItem>
+              <ListItem>Watching Movies & Web Series</ListItem>
+              <ListItem>Bike Rides</ListItem>
+            </UnorderedList>
+          </Text>
         </Box>
-        <Box></Box>
+        <Box width="40%">
+            <Img src="" alt="" />
+        </Box>
       </Flex>
     </Box>
   );
