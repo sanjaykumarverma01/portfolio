409ecefc0c6b282b4eb632755b2e96ead694ef01
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2022-08-14 16:12:45 +0530

    Added Home png

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,21 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import About from "./About";
 import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Skills from "./Skills";
 
 const Home = () => {
   return (
     <>
+      <Flex>
+        <Box>Details</Box>
+        <Box>Img</Box>
+      </Flex>
       <Navbar />
+      <About />
+      <Skills />
+      <Projects />
     </>
   );
 };
